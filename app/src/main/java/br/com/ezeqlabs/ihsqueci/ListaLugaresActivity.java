d8cc81ae8b29484d04fb9b97d548834793323724
Kako Botasso <kakodev@gmail.com>
2016-02-21 10:22:01 -0300

    Commit inicial

---
+++ app/src/main/java/br/com/ezeqlabs/ihsqueci/ListaLugaresActivity.java
@@ -0,0 +1,13 @@
+package br.com.ezeqlabs.ihsqueci;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaLugaresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_lugares);
+    }
+}
